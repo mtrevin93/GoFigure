6fe84c70bd07faf526197c50fbb6b86e8a924795
mtrevin93 <mtrevin93@gmail.com>
2021-07-23 16:53:46 -0400

    login added, bulma navbar not displaying properly

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -4,6 +4,21 @@ import { Route } from "react-router-dom"
 export const ApplicationViews = () => {
     return (
         <>
+         <Route path="/home">
+
+         </Route>
+
+         <Route path="/bricks">
+
+         </Route>
+
+         <Route path="/build">
+
+         </Route>
+
+         <Route path="/collection">
+             
+         </Route>
         </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './main.css';
 import { BrowserRouter as Router } from "react-router-dom";
 import { GoFigure } from "./components/GoFigure";
 
