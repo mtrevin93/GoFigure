1082945ef4054c20b6353bcfd22928f7cf14a7ae
mtrevin93 <mtrevin93@gmail.com>
2021-07-23 10:50:47 -0400

    added login/auth

---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,14 @@
+
+
+export const NavBar = () => {
+  return (
+
+<section className="section">
+ <div className="container">
+   <h1 className="title"> Hello World </h1>
+   <p className="subtitle"> My React app  with     <strong>Bulma</strong> </p>
+ </div>
+</section>
+  )
+}
+
